6475b4661e549eaf4ffe22bc3205dc3be2f12a55
Juico Bowley <68080175+Juicob@users.noreply.github.com>
2020-12-02 17:19:53 -0500

    trained initial model

--- notebook-1.py
+++ notebook-1.py
@@ -8,6 +8,9 @@ from tqdm import tqdm
 import tensorflow as tf
 
 from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import RMSprop
+
 
 # %% [markdown]
 ## Setting up paths
@@ -24,7 +27,6 @@ val_pneumonia = os.path.join('../project_image_data/val/PNEUMONIA')
 
 all_paths = [train_normal, train_pneumonia, test_normal, test_pneumonia, val_normal, val_pneumonia]
 # %%
-# train_normal_files = os.listdir(train_normal)
 train_normal_files = [file for file in os.listdir(train_normal) if file.endswith('.jpeg')]
 train_pneumonia_files = [file for file in os.listdir(train_pneumonia) if file.endswith('.jpeg')]
 
@@ -65,7 +67,95 @@ for i, img_path in enumerate(next_normal_pix+next_pneumonia_pix):
 
 plt.show()
 # %%
+model = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 300x300 with 3 bytes color
+    # This is the first convolution
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(300, 300, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The third convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fifth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('horses') and 1 for the other ('humans')
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+# %%
+model.summary()
+# %%
+ 
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['accuracy'])
+# %%
+# All images will be rescaled by 1./255
+img_datagen = ImageDataGenerator(rescale=1./255)
+ 
+# Flow training images in batches of 128 using train_datagen generator
+train_generator = img_datagen.flow_from_directory(
+        '../project_image_data/train/',  # This is the source directory for training images
+        target_size=(300, 300),  # All images will be resized to 150x150
+        batch_size=128,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
 
+val_generator = img_datagen.flow_from_directory('../project_image_data/val/', 
+        target_size=(300, 300),  # All images will be resized to 150x150
+        batch_size=128,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+# %%
+val_generator
+# %%
+history = model.fit(
+      train_generator,
+      steps_per_epoch=8,  
+      epochs=3, #epochs=15
+      verbose=1,
+      validation_data=val_generator)
+
+# %%
+result = model.evaluate(test)
+dict(zip(model.metrics_names, result))
+# %%
+history.history
+# %%
+#-----------------------------------------------------------
+# Retrieve a list of list results on training and test data
+# sets for each training epoch
+#-----------------------------------------------------------
+accuracy=history.history['accuracy']
+val_acc=history.history['val_accuracy']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+ 
+epochs=range(len(accuracy)) # Get number of epochs
+ 
+#------------------------------------------------
+# Plot training and validation accuracy per epoch
+#------------------------------------------------
+plt.plot(epochs, accuracy, 'r', "Training Accuracy")
+plt.plot(epochs, val_acc, 'b', "Validation Accuracy")
+plt.title('Training and validation accuracy')
+plt.figure()
+ 
+#------------------------------------------------
+# Plot training and validation loss per epoch
+#------------------------------------------------
+plt.plot(epochs, loss, 'r', "Training Loss")
+plt.plot(epochs, val_loss, 'b', "Validation Loss")
+plt.figure()
 # %%
 
 # %%
