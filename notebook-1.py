67eac1505466bd07b416e624dc651218a7193d22
Juico Bowley <68080175+Juicob@users.noreply.github.com>
2020-12-08 18:12:35 -0500

    more iterations

--- notebook-1.py
+++ notebook-1.py
@@ -64,7 +64,7 @@ def plotImages(images_arr, labels_arr):
     plt.show()
 
 def plot_confusion_matrix(cm,
-                          normalize=False,
+                          normalize=True,
                           title='Confusion matrix',
                           cmap=plt.cm.Greens):
     """
@@ -546,5 +546,46 @@ plot_confusion_matrix(cm)
 # %% [markdown]
 ### Use LIME to show 'feature' selection in a sense
 # %%
+Adam_32_32_32_32_2D = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 300x300 with 3 bytes color
+    # This is the first convolution
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(64, 64, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
 
+
+    tf.keras.layers.Flatten(),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(64, activation='relu'),
+    # Only 1 output neuron. It will contain a value from 0-1
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+# %%
+Adam_32_32_32_32_2D.summary()
+# %%
+Adam_32_32_32_32_2D.compile(loss='binary_crossentropy',
+              optimizer="Adam",
+              metrics=['accuracy'])
+
+# %%
+%%time
+tensorboard = TensorBoard(log_dir=f'./logs/Adam_32_32_32_32_2D_{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}')
+history9 = Adam_32_32_32_32_2D.fit(
+      train_generator,
+    #   steps_per_epoch=8,  
+      epochs=100, #epochs=15
+      verbose=1,
+      callbacks=[earlystop, tensorboard],
+      validation_data=val_generator)
+
+# %%
+plot_results(history9.history)
+cm = prepare_confusion_matrix(Adam_32_32_32_32_2D)
+plot_confusion_matrix(cm)
 # %%
